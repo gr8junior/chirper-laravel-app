9338b9c95467ece5a40f20244bd0dea4825f47c0
Sampson Aidoo <samqwame10@gmail.com>
2025-10-21 11:40:41 +0000

    Text change to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:title>
+        Welcome 
+    </x-slot:title>
 <div class="max-w-2xl mx-auto">
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
@@ -6,6 +9,7 @@
                         <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
                         <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
                             sing (or chirp)!</p>
+                            <p class="mt-2 text-sm text-gray-600">This is now live on the internet </p>
                     </div>
                 </div>
             </div>
