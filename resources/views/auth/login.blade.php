133f09a19108328df39ae8ab35f60d559c09a471
Sampson Aidoo <samqwame10@gmail.com>
2025-10-23 13:21:42 +0000

    Created the the sign in and sign up page with designs

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,62 @@
+
+<x-layout>
+    <x-slot:title>
+        Sign In
+    </x-slot:title>
+
+    <div class="hero min-h-[calc(100vh-16rem)]">
+        <div class="hero-content flex-col">
+            <div class="card w-96 bg-base-100">
+                <div class="card-body">
+                    <h1 class="text-3xl font-bold text-center mb-6">Sign In</h1>
+
+                    <form method="POST" action="/register">
+                        @csrf
+
+                        <!-- Name -->
+                        
+                        <!-- Email -->
+                        <label class="floating-label mb-6">
+                            <input type="email"
+                                   name="email"
+                                   placeholder="[mail@example.com](<mailto:mail@example.com>)"
+                                   value="{{ old('email') }}"
+                                   class="input input-bordered @error('email') input-error @enderror"
+                                   required>
+                            <span>Email</span>
+                        </label>
+                        @error('email')
+                            <div class="label -mt-4 mb-2">
+                                <span class="label-text-alt text-error">{{ $message }}</span>
+                            </div>
+                        @enderror
+
+                        <!-- Password -->
+                        <label class="floating-label mb-6">
+                            <input type="password"
+                                   name="password"
+                                   placeholder="••••••••"
+                                   class="input input-bordered @error('password') input-error @enderror"
+                                   required>
+                            <span>Password</span>
+                        </label>
+                        @error('password')
+                            <div class="label -mt-4 mb-2">
+                                <span class="label-text-alt text-error">{{ $message }}</span>
+                            </div>
+                        @enderror
+
+                       
+
+                        <!-- Submit Button -->
+                        <div class="form-control mt-8">
+                            <button type="submit" class="btn btn-primary btn-sm w-full">
+                                login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, ' destroy']);
 
+Route::view('/register', 'auth.register');
+Route::view('/login', 'auth.login');
+
+
 /*Route::get('/sign-in', function () {
     return view('sign-in');
 })->name('signin');
